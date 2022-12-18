e9fd32b22ebae10ec1fb0f0c0da8c5a37f2d1b3a
Vincent <thisoneis4business@gmail.com>
2022-12-18 22:41:27 +1300

    Finished for now

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,14 +3,33 @@
   windows_subsystem = "windows"
 )]
 
+#[derive(serde::Serialize)]
+struct CustomResponse {
+  message: String,
+}
+
+async fn something_else() -> Option<String> {
+  Some("All goods from the frontend 👍".into())
+}
+
 #[tauri::command]
-fn ok() -> String {
-  String::from("All goods here 👍")
+async fn something(
+  window: tauri::Window
+) -> Result<CustomResponse, String> {
+  println!("All goods here 👍 at native level, passing back to {}", window.label());
+  let result: Option<String> = something_else().await;
+  if let Some(message) = result {
+    Ok(CustomResponse {
+      message
+    })
+  } else {
+    Err("No result".into())
+  }
 }
 
 fn main() {
   tauri::Builder::default()
-    .invoke_handler(tauri::generate_handler![ok])
+    .invoke_handler(tauri::generate_handler![something])
     .run(tauri::generate_context!())
     .expect("error while running tauri application");
 }
